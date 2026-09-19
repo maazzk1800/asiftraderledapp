842ffe4ef71912d19554cb4f2902e5db94edc272
ledger.01 <tabrez.khan@iconicsoft.net>
2026-09-19 23:22:13 +0530

    Brand Windows release as Asiftraderledapp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:asif_traders_ledapp/main.dart';
+import 'package:asiftraderledapp/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
